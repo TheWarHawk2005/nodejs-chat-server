1151d9a7d2abd294481903d0f34e1b50bbdb72ca
TheWarHawk2005 <56003863+TheWarHawk2005@users.noreply.github.com>
2022-02-25 21:18:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 const server = require('http').createServer(app)
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const io = require('socket.io')(server)
 const path = require('path')
 
